a6597a132549ee72c3816a88f3421721dea91550
Ingrid Midori <ingrid.midori@brunsker.com.br>
2024-04-08 18:33:49 -0300

    deploy

--- src/app/home/about/about.component.ts
+++ src/app/home/about/about.component.ts
@@ -2,13 +2,13 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-about',
-  templateUrl: './about.component.html'
+  templateUrl: './about.component.html',
 })
 export class AboutComponent implements OnInit {
-
-  constructor() { }
+  year: number;
+  constructor() {}
 
   ngOnInit() {
+    this.year = new Date().getFullYear()
   }
-
 }
